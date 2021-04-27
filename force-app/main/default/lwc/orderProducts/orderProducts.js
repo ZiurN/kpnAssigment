401c4e67315ea210b26d07c79386554ba2c3a7e2
Jeferson Ruiz <jefryruiz@gmail.com>
2021-04-26 22:37:57 -0400

    feat(Order Products CMP)
    Added  OrderItemAddedEvent event listener and handler.
    
    It seems that the apex.refresh, and even the re-invoke of the Apex method
    to update the datatable info doesn't work if the initial invoke is done via
    @wire decorator. To solve this, the apex method to retrieve de data of the
    order item is invoke imperatively.
    
    As result, remove the (cacheable=true) param of the AuraEnabled annotation
    is needed.
    
    Finally, to recharge the data in tne table, a spinner is added in the
    html file, to show it while the table is rebuilted.

--- force-app/main/default/lwc/orderProducts/orderProducts.js
+++ force-app/main/default/lwc/orderProducts/orderProducts.js
@@ -1,8 +1,16 @@
-import { LightningElement, wire, api } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getOrderItems from '@salesforce/apex/OrderProductsController.getOrderItems';
 import activateOrder from '@salesforce/apex/OrderProductsController.activateOrder';
+/** To listen the event orderItemsAddedEvent from the availableProducts component */
 import { getRecord, getRecordNotifyChange } from 'lightning/uiRecordApi';
+import {
+    subscribe,
+    unsubscribe,
+    APPLICATION_SCOPE,
+    MessageContext
+} from 'lightning/messageService';
+import orderItemsAddedEvent from '@salesforce/messageChannel/orderItemsAddedEvent__c';
 const columns = [
 	{
 		label: 'Name',
@@ -22,111 +30,151 @@ const columns = [
 	{ label: 'Total Price', fieldName: 'TotalPrice', sortable: true, type: 'currency', hideDefaultActions : 'true' }
 ];
 export default class OrderProducts extends LightningElement {
-	orderStatus = 'Draft';
-	activatingOrder = false;
-	areDetailsVisible = false;
-	columns = columns;
-	dataTable = [];
-	initialOffset = 10
-	initialData = [];
-	defaultSortDirection = 'asc';
-	sortDirection = 'asc';
-	sortedBy;
-	@api recordId;
-	@wire(getRecord, {recordId : '$recordId', fields: ['Order.Status']})
-	getRecordCallBack({error, data}){
-		if(data){
-			this.orderStatus = data.fields.Status.value
-			this.activatingOrder = this.orderStatus === 'Activated';
-		}else if(error){
-			console.log(error);
-		}
-	}
-	@wire(getOrderItems, {OrderId : '$recordId'})
-	wiredProject({error, data}){
-		if (data) {
-			if(data.length > 0){
-				data.forEach((orderItem, idx) => {
-					let dataToTable = {};
-					dataToTable.Id = orderItem.Id;
-					dataToTable.Name = orderItem.Product2.Name;
-					dataToTable.UnitPrice = orderItem.UnitPrice;
-					dataToTable.Link = '/' + orderItem.Id;
-					dataToTable.Quantity = orderItem.Quantity;
-					dataToTable.TotalPrice = orderItem.TotalPrice;
-					this.dataTable.push(dataToTable);
-					if(idx < this.initialOffset){
-						this.initialData.push(dataToTable);
-					}
-				});
+	/** View controller attributes */
+		orderStatus = 'Draft';
+		activatingOrder = false;
+		areDetailsVisible = false;
+	/** dataTable controller attributes */
+		columns = columns;
+		dataTable = [];
+		initialOffset = 10
+		initialData = [];
+		defaultSortDirection = 'asc';
+		sortDirection = 'asc';
+		sortedBy;
+	/** -- */
+		@api recordId;
+	/** to get the status of the order and control activation button availability */
+		@wire(getRecord, {recordId : '$recordId', fields: ['Order.Status']})
+		getRecordCallBack({error, data}){
+			if(data){
+				this.orderStatus = data.fields.Status.value
+				this.activatingOrder = this.orderStatus === 'Activated';
+			}else if(error){
+				console.log(error);
 			}
-		}else if(error){
-			console.log(error);
 		}
-		if(this.dataTable.length > 0){
-			this.areDetailsVisible = true;
+	/** to listen the event */
+		subscription = null;
+		@wire(MessageContext) messageContext;
+	/**
+	 * Because the @wire decoration doesn't work to update the table when the event is
+	 * listen, the option is to invoke the apex method to get the data imperatively.
+	 */
+		connectedCallback() {
+			this.subscribeToMessageChannel();
+			this.getOrderItemsMethod();
 		}
-	}
-	activateOrder() {
-		this.activatingOrder = true;
-		let OrderId = this.recordId;
-		activateOrder({OrderId: OrderId})
+	/** wrapper method of the apex invocation */
+		getOrderItemsMethod(){
+			let dataTable = [];
+			let initialData = [];
+			getOrderItems({OrderId : this.recordId})
 				.then(result => {
-					this.sendMessageToUser(result.status, result.message);
-					getRecordNotifyChange([{recordId: OrderId}]);
-					if(result.orderStatus != 'Activated'){
-						this.activatingOrder = false;
+					if(result.length > 0){
+						result.forEach((orderItem, idx) => {
+							let dataToTable = {};
+							dataToTable.Id = orderItem.Id;
+							dataToTable.Name = orderItem.Product2.Name;
+							dataToTable.UnitPrice = orderItem.UnitPrice;
+							dataToTable.Link = '/' + orderItem.Id;
+							dataToTable.Quantity = orderItem.Quantity;
+							dataToTable.TotalPrice = orderItem.TotalPrice;
+							dataTable.push(dataToTable);
+							if(idx < this.initialOffset){
+								initialData.push(dataToTable);
+							}
+							console.log(dataToTable);
+						});
 					}
+					this.dataTable = dataTable;
+					this.initialData = initialData;
+					this.areDetailsVisible = true;
 				}).catch(error => {
 					console.log(error);
-					this.sendMessageToUser('error', 'Error activating the Order');
-					this.activatingOrder = false;
+					this.sendMessageToUser('error', 'Error loading');
+					this.areDetailsVisible = true;
 				});
-	}
-	sortBy(field, reverse) {
-		return function(dataA, dataB) {
-			field = field == 'Link' ? 'Name' : field;
-			let detailA = dataA[field];
-			let detailB = dataB[field];
-			if(detailA == detailB){
-				return 0;
-			}else {
-				return (detailA > detailB ? 1 : -1) * reverse;
-			}
-		};
-	}
-	sortTable(event) {
-		const { fieldName: sortedBy, sortDirection } = event.detail;
-		const cloneData = [...this.initialData];
-		cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
-		this.initialData = cloneData;
-		this.sortDirection = sortDirection;
-		this.sortedBy = sortedBy;
-	}
-	loadMoreProducts(){
-		let totalElements = this.dataTable.length;
-		let offSet = this.initialOffset;
-		let currentVisibleElements = this.initialData.length;
-		if(totalElements === currentVisibleElements){
-			return;
 		}
-		if(totalElements < offSet){
-			this.initialData = this.dataTable;
-			return;
-		}else{
-			let nextOffSet = currentVisibleElements + offSet;
-			if(totalElements < nextOffSet){
+		activateOrder() {
+			this.activatingOrder = true;
+			let OrderId = this.recordId;
+			activateOrder({OrderId: OrderId})
+					.then(result => {
+						this.sendMessageToUser(result.status, result.message);
+						getRecordNotifyChange([{recordId: OrderId}]);
+						if(result.orderStatus != 'Activated'){
+							this.activatingOrder = false;
+						}
+					}).catch(error => {
+						console.log(error);
+						this.sendMessageToUser('error', 'Error activating the Order');
+						this.activatingOrder = false;
+					});
+		}
+	/** to control datatable behavior */
+		sortBy(field, reverse) {
+			return function(dataA, dataB) {
+				field = field == 'Link' ? 'Name' : field;
+				let detailA = dataA[field];
+				let detailB = dataB[field];
+				if(detailA == detailB){
+					return 0;
+				}else {
+					return (detailA > detailB ? 1 : -1) * reverse;
+				}
+			};
+		}
+		sortTable(event) {
+			const { fieldName: sortedBy, sortDirection } = event.detail;
+			const cloneData = [...this.initialData];
+			cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
+			this.initialData = cloneData;
+			this.sortDirection = sortDirection;
+			this.sortedBy = sortedBy;
+		}
+		loadMoreProducts(){
+			let totalElements = this.dataTable.length;
+			let offSet = this.initialOffset;
+			let currentVisibleElements = this.initialData.length;
+			if(totalElements === currentVisibleElements){
+				return;
+			}
+			if(totalElements < offSet){
 				this.initialData = this.dataTable;
+				return;
 			}else{
-				this.initialData = this.initialData.concat(this.dataTable.slice(currentVisibleElements, nextOffSet));
+				let nextOffSet = currentVisibleElements + offSet;
+				if(totalElements < nextOffSet){
+					this.initialData = this.dataTable;
+				}else{
+					this.initialData = this.initialData.concat(this.dataTable.slice(currentVisibleElements, nextOffSet));
+				}
 			}
 		}
-	}
-	sendMessageToUser(status, message){
-		const evt = new ShowToastEvent({
-			message: message,
-			variant: status,
-		});
-		this.dispatchEvent(evt);
-	}
+	/** to listen and handle the order added event */
+		subscribeToMessageChannel() {
+			if (!this.subscription) {
+				this.subscription = subscribe(
+					this.messageContext,
+					orderItemsAddedEvent,
+					(message) => this.handleMessage(),
+					{ scope: APPLICATION_SCOPE }
+				);
+			}
+		}
+		handleMessage() {
+			this.activatingOrder = true;
+			this.areDetailsVisible = false;
+			this.getOrderItemsMethod();
+			this.activatingOrder = false;
+			this.areDetailsVisible = true;
+		}
+		sendMessageToUser(status, message){
+			const evt = new ShowToastEvent({
+				message: message,
+				variant: status,
+			});
+			this.dispatchEvent(evt);
+		}
 }
